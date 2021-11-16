697ae5d11e86fbce9d0ba7254fbe3b89a3b95ac4
bipukatuwal7 <bipukatuwal7>
2021-11-16 19:32:36 +0545

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,6 @@ const Buysell = require("../models/buysell");
 
 const app = express();
 const cors = require("cors");
-/* const StockName = require("../models/stockname");
-const TransactionType = require("../models/transactiontype"); */
 
 app.use(cors());
 app.use(express.json());
@@ -23,24 +21,6 @@ Buysell.find()
   .then((c) => {
     console.log(c);
   });
-/* app.post("/stockname", (req, res) => {
-  StockName.create(req.body)
-    .then((stockname) => {
-      res.status(200).send(stockname);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-app.post("/transactiontype", (req, res) => {
-  TransactionType.create(req.body)
-    .then((transactiontype) => {
-      res.status(200).send(transactiontype);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-}); */
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
